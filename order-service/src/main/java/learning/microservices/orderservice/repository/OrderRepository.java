fd71cbe56616646b834eb6bf2ff7ae3af8cfa795
HossameTor <hossametorchi07@gmail.com>
2023-11-28 05:05:15 +0100

    [add] order service

---
+++ order-service/src/main/java/learning/microservices/orderservice/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package learning.microservices.orderservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import learning.microservices.orderservice.model.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>{
+    
+}
